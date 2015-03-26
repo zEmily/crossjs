6cc1aa68fd3590f91f7d5ab2e6eb6d1b24851b9f
zEmily <ademily@163.com>
2015-03-26 12:33:46 +0800

    测试第三方暗黄

--- cloud/app.js
+++ cloud/app.js
@@ -1,5 +1,7 @@
 // 在 Cloud code 里初始化 Express 框架
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	nib = require('nib')
 var app = express();
 
 // App 全局配置
@@ -7,6 +9,18 @@ app.set('views','cloud/views');   // 设置模板目录
 app.set('s','cloud/public');   // 设置static目录
 app.set('view engine', 'jade');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
+
+// 配置stylus
+app.use(stylus.middleware({
+	src: 'w',
+	dest: 'public',
+	compile: function compile(str, path) {
+		return stylus(str)
+			.set('filename', path)
+			.set('compress', true)
+			.use(nib());
+	}
+}));
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 var params = {}
@@ -17,7 +31,6 @@ params.date = {
 	'date': today.getDate(),
 	'day': today.getDay()
 }
-console.log(params);
 app.get('/hello', function(req, res) {
   res.render('hello', { params: params });
 });
