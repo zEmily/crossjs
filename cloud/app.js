a2c013af40daec0d5d51fdd072eebe8346263388
zEmily <ademily@163.com>
2015-03-26 16:19:19 +0800

    再来

--- cloud/app.js
+++ cloud/app.js
@@ -7,7 +7,7 @@ var app = express();
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('s','cloud/public');   // 设置static目录
-app.set('view engine', 'jade');    // 设置 template 引擎
+app.set('view engine', 'jade');
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 配置stylus
@@ -34,7 +34,9 @@ params.date = {
 app.get('/hello', function(req, res) {
   res.render('hello', { params: params });
 });
-
+app.get('/test', function(req, res) {
+	res.render('test', {params: params});
+})
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
